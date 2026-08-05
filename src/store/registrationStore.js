3c73617358a3d611782caee6c2eadc44f02c04e6
ChuchoGodDeveloper <loij030909hmcpssa0@gmail.com>
2026-08-05 01:34:03 -0600

    fix: TipoLicencia 'Grupal' with capital G

--- src/store/registrationStore.js
+++ src/store/registrationStore.js
@@ -3,14 +3,14 @@ import { create } from 'zustand'
 const useRegistrationStore = create((set) => ({
   usuarioId: null,
   correo: '',
-  tipoLicencia: 'grupal',
+  tipoLicencia: 'Grupal',
   licenciaId: null,
   step1Completed: false,
   step2Completed: false,
   setUsuarioData: (usuarioId, correo) => set({ usuarioId, correo, step1Completed: true }),
   setStep2Completed: () => set({ step2Completed: true }),
   setLicenciaId: (licenciaId) => set({ licenciaId }),
-  clearRegistration: () => set({ usuarioId: null, correo: '', tipoLicencia: 'grupal', licenciaId: null, step1Completed: false, step2Completed: false }),
+  clearRegistration: () => set({ usuarioId: null, correo: '', tipoLicencia: 'Grupal', licenciaId: null, step1Completed: false, step2Completed: false }),
 }))
 
 export default useRegistrationStore
